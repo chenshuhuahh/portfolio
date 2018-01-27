44b5fbab9cbea7cccbca97d8b0b20b820d3623ea
hua <1124203375@qq.com>
2018-01-28 00:36:43 +0800

    Add mobile section style

--- src/components/summary/summary.vue
+++ src/components/summary/summary.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-    <section class="topBlock" ref="topBlockSec" :style="{backgroundPosition: '50% -'+ topBlockPosition/3 + 'px'}">
+    <section class="topBlock" ref="topBlockSec" :style="{backgroundPositionY: '-' + topBlockPosition/3 + 'px'}">
       <div class="mainContainer">
         <h1>Grow Your Brand,<br/>Get Published.</h1>
       </div>
     </section>
     <section class="smallBlock">
       <span>今天就开始创建您的免费在线作品展示网站。</span>
-      <div class="button ">立即开始</div>
+      <div class="button">立即开始</div>
     </section>
     <section class="nicheBlock"></section>
   </div>
@@ -15,7 +15,7 @@
 
 <script type="text/ecmascript-6">
 export default {
-  data () {
+  data() {
     return {
       topBlockPosition: 0
     };
@@ -25,7 +25,7 @@ export default {
       this.topBlockPosition = document.body.scrollTop;
     }
   },
-  created() {
+  mounted() {
     window.addEventListener('scroll', this.getTopBlock);
   }
 };
@@ -36,7 +36,8 @@ export default {
   margin-top: 50px;
   height: 640px;
   background-image: url(bg01.jpg);
-  background-position: center bottom;
+  background-position-x: 60%;
+  background-position-y: bottom;
   background-attachment: fixed;
   background-repeat: no-repeat;
   position: relative;
@@ -53,21 +54,20 @@ export default {
     }
   }
 }
-
 .smallBlock {
   background-color: #27272b;
   text-align: center;
   color: gray;
   font-family: proxima_novasemibold,'Helvetica Neue',Helvetica,Arial,sans-serif;
   font-size: 14px;
-  padding: 12px 20px;
+  padding: 20px;
   span {
     color: #fff;
   }
   .button {
     display: inline-block;
     color: #44b2e2;
-    margin: 0 0 0 10px;
+    margin-top: 10px;
     padding: 6px 12px;
     border: 1px solid #44b2e2;
     cursor: pointer;
@@ -76,15 +76,25 @@ export default {
     border-radius: 3px;
   }
 }
-
 .nicheBlock {
   background-image: url(bg02.jpg);
   background-size: cover;
   background-position: top center;
   text-align: center;
   padding: 0 20px;
-  width: 100%;
   height: 200px;
+}
+
+@media (min-width: 768px) {
+  .topBlock {
+    background-position-x: 50%;
+  }
+  .smallBlock {
+    padding: 12px 20px;
+    .button {
+      margin-right: 10px;
+    }
+  }
 }
 
 </style>
--- src/components/topNav/topNav.vue
+++ src/components/topNav/topNav.vue
@@ -1,97 +1,183 @@
 <template>
-  <nav class="topNav">
-    <div class="mainContainer">
-      <div class="logo">logo</div>
-      <ul class="actionMenu">
-        <li class="signUp"><a href="">Sign Up</a></li>
-        <li class="logIn"><a href="">Log In</a></li>
-      </ul>
-      <ul class="mainMenu">
-        <li><a href="" class="active">概述</a></li>
-        <li><a href="">作品栏</a></li>
-        <li><a href="">学生/企业</a></li>
-      </ul>
-    </div>
-  </nav>
+  <div>
+    <nav class="topNav">
+      <div class="mainContainer">
+        <div class="logo">logo</div>
+        <ul class="actionMenu">
+          <li class="signUp"><a href="">Sign Up</a></li>
+          <li class="logIn"><a href="">Log In</a></li>
+          <li class="foldMenu" @click="toggleMenu" :class="{active: isShowMobileMenu}"><i class="el-icon-minus"></i></li>
+        </ul>
+        <ul class="mainMenu">
+          <li><a href="" class="active">概述</a></li>
+          <li><a href="">作品栏</a></li>
+          <li><a href="">学生/企业</a></li>
+        </ul>
+      </div>
+    </nav>
+    <transition name="fold">
+      <nav class="mobileMenu" v-show="isShowMobileMenu">
+        <ul>
+          <li><a href="" class="active">概述</a></li>
+          <li><a href="">作品栏</a></li>
+          <li><a href="">学生/企业</a></li>
+        </ul>
+      </nav>
+    </transition>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
-export default {
-  data () {
-    return {};
-  }
-};
+  export default {
+    data () {
+      return {
+        isShowMobileMenu: false
+      };
+    },
+    methods: {
+      toggleMenu() {
+        this.isShowMobileMenu = !this.isShowMobileMenu;
+        if (this.isShowMobileMenu) {
+          this.$refs.foldMenu.style.color = '#44b2e2';
+        }
+      }
+    }
+  };
 </script>
 
 <style lang="scss" type="text/scss">
-.topNav {
-  font-size: 14px;
-  background-color: #27272b;
-  padding: 18px 0 16px;
-  -webkit-box-shadow: 0 7px 47px -10px rgba(0,0,0,.75);
-  -moz-box-shadow: 0 7px 47px -10px rgba(0,0,0,.75);
-  box-shadow: 0 7px 47px -10px rgba(0,0,0,.75);
-  position: fixed;
-  top: 0;
-  width: 100%;
-  z-index: 200;
-  color: #fff;
-  ul {
-    list-style: none;
-  }
-  .mainContainer {
-    max-width: 100%;
-    margin: 0 auto;
-  }
-}
-.logo {
-  width: 100px;
-  height: 20px;
-  float: left;
-  margin-left: 20px;
-  background: #f82d2d;
-}
-.actionMenu {
-  float: right;
-  margin: 0 20px 0 0;
-  li {
-    display: block;
-    float: left;
-    color: gray;
-    text-transform: uppercase;
-    padding: 0 10px;
-    border-right: 1px solid gray;
-    a {
-      text-decoration: none;
-      color: #bebebe;
-      &:hover {
-        color: #44b2e2;
+  .topNav {
+    font-size: 14px;
+    background-color: #27272b;
+    padding: 18px 0 16px;
+    -webkit-box-shadow: 0 7px 47px -10px rgba(0, 0, 0, .75);
+    -moz-box-shadow: 0 7px 47px -10px rgba(0, 0, 0, .75);
+    box-shadow: 0 7px 47px -10px rgba(0, 0, 0, .75);
+    position: fixed;
+    top: 0;
+    width: 100%;
+    z-index: 200;
+    color: #fff;
+    ul {
+      list-style: none;
+    }
+    .mainContainer {
+      max-width: 100%;
+      margin: 0 auto;
+      .logo {
+        width: 100px;
+        height: 20px;
+        float: left;
+        margin-left: 20px;
+        background: #f82d2d;
+      }
+      .actionMenu {
+        float: right;
+        margin: 0 5px 0 0;
+        li {
+          display: block;
+          float: left;
+          color: gray;
+          text-transform: uppercase;
+          padding: 0 10px;
+          border-left: 1px solid gray;
+          &:first-child {
+            border: none;
+          }
+          &:last-child {
+            display: block;
+          }
+          &.active {
+            color: #44b2e2;
+          }
+          a {
+            text-decoration: none;
+            color: #bebebe;
+          }
+        }
+        .logIn {
+          padding-left: 7px;
+        }
+      }
+      .mainMenu {
+        display: none;
       }
     }
   }
-  .logIn {
-    padding-left: 7px;
-  }
-}
-.mainMenu {
-  text-align: center;
-  margin: 3px 0 0;
-  li {
-    display: inline;
-    padding-right: 27px;
-    text-transform: uppercase;
-    a {
-      text-decoration: none;
-      color: gray;
-      padding-bottom: 6px;
-      &:hover {
-        color: #bebebe;
-      }
-      &.active {
-        color: #bebebe;
-        border-bottom: 2px solid #44b2e2;
+
+  .mobileMenu {
+    z-index: 100;
+    width: 132px;
+    height: 100%;
+    background-color: #27272b;
+    position: fixed;
+    right: -210px;
+    top: 55px;
+    transform: translate3d(-150%, 0, 0);
+    &.fold-enter-active, &.fold-leave-active {
+      transition: all 0.8s;
+    }
+    &.fold-enter, &.fold-leave-active{
+      transform: translate3d(0, 0, 0);
+    }
+    ul {
+      padding: 20px;
+      font-size: 12px;
+      li {
+        display: block;
+        padding-bottom: 27px;
+        a {
+          text-decoration: none;
+          color: gray;
+          padding-bottom: 5px;
+          &.active {
+            color: #bebebe;
+            border-bottom: 2px solid #44b2e2;
+          }
+        }
       }
     }
   }
-}
+
+  @media (min-width: 768px) {
+    .topNav {
+      .mainContainer {
+        .actionMenu {
+          margin: 0 20px 0 0;
+          li {
+            &:last-child {
+              display: none;
+            }
+            a {
+              &:hover {
+                color: #44b2e2;
+              }
+            }
+          }
+        }
+        .mainMenu {
+          display: block;
+          text-align: center;
+          margin: 3px 0 0;
+          li {
+            display: inline;
+            padding-right: 27px;
+            text-transform: uppercase;
+            a {
+              text-decoration: none;
+              color: gray;
+              padding-bottom: 6px;
+              &:hover {
+                color: #bebebe;
+              }
+              &.active {
+                color: #bebebe;
+                border-bottom: 2px solid #44b2e2;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
 </style>
